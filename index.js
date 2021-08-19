350e5f85db256956673d35fe1d751b4a993f3ddc
ltrzalez <ltrzalez@gmail.com>
2021-08-18 22:01:16 -0300

    feature msg

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-console.log('feature')
-console.log('feature')
-console.log('feature')
+let arr = ['hola ', 'como ', 'estas?']
+let msg = ''
+arr.forEach(el => {
+    msg += el
+} )
+console.log(msg)
